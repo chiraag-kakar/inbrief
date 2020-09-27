d90f946a220a57ee195b491c0bea500fc5617ba1
chiraag-kakar <chirucos1998@gmail.com>
2020-09-27 14:08:37 +0530

    Updates

---
+++ precache-manifest.2da5b01c105fb468f24038e76b6d5e77.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f804bab4545a0ffce35a245af2a42867",
+    "url": "/inbrief/index.html"
+  },
+  {
+    "revision": "55cdd211976448d1c427",
+    "url": "/inbrief/static/css/4.e9b04e07.chunk.css"
+  },
+  {
+    "revision": "ba242881b5570fce6dba",
+    "url": "/inbrief/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2db5a8bae8d2d02a2ab6",
+    "url": "/inbrief/static/js/0.83579669.chunk.js"
+  },
+  {
+    "revision": "2b9f365654b90af03af5",
+    "url": "/inbrief/static/js/10.5ac36b2a.chunk.js"
+  },
+  {
+    "revision": "555afb49d618d20c5325",
+    "url": "/inbrief/static/js/11.62008511.chunk.js"
+  },
+  {
+    "revision": "b70cc80e6af2592ab87e",
+    "url": "/inbrief/static/js/12.d7302add.chunk.js"
+  },
+  {
+    "revision": "40f90b66de4b75d32de2",
+    "url": "/inbrief/static/js/13.2eced533.chunk.js"
+  },
+  {
+    "revision": "55b0b9486d1a1657fa85",
+    "url": "/inbrief/static/js/14.d38a54b7.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/inbrief/static/js/14.d38a54b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc04a6312b5e9065d675",
+    "url": "/inbrief/static/js/15.b26020f4.chunk.js"
+  },
+  {
+    "revision": "c2031a646cc531d76039",
+    "url": "/inbrief/static/js/16.905d3f1b.chunk.js"
+  },
+  {
+    "revision": "57c93cae21b5f12fb194",
+    "url": "/inbrief/static/js/17.751e5451.chunk.js"
+  },
+  {
+    "revision": "4534797057b8adc31f3f",
+    "url": "/inbrief/static/js/18.e3e5e4f0.chunk.js"
+  },
+  {
+    "revision": "420d6b7d07d46bd11181",
+    "url": "/inbrief/static/js/19.133135af.chunk.js"
+  },
+  {
+    "revision": "2ffb1c0e41167d5d3c39",
+    "url": "/inbrief/static/js/20.c6d1fb9f.chunk.js"
+  },
+  {
+    "revision": "2880effc4ba235fe01aa",
+    "url": "/inbrief/static/js/21.c8af671e.chunk.js"
+  },
+  {
+    "revision": "52d9f051a41b36247bb2",
+    "url": "/inbrief/static/js/22.4fb1651a.chunk.js"
+  },
+  {
+    "revision": "c59a3952fcf56aab24ab",
+    "url": "/inbrief/static/js/23.52a62f3a.chunk.js"
+  },
+  {
+    "revision": "2a24ff4dd93a9997871a",
+    "url": "/inbrief/static/js/24.d1d62ff7.chunk.js"
+  },
+  {
+    "revision": "e77bd551ad13fa0d2722",
+    "url": "/inbrief/static/js/25.c5be1193.chunk.js"
+  },
+  {
+    "revision": "e4b815965410bcbd787d",
+    "url": "/inbrief/static/js/26.0439e678.chunk.js"
+  },
+  {
+    "revision": "8e922d7605c07f2ab40c",
+    "url": "/inbrief/static/js/27.a7433f46.chunk.js"
+  },
+  {
+    "revision": "0c4789f479fc7ae4a24c",
+    "url": "/inbrief/static/js/28.b7570a9f.chunk.js"
+  },
+  {
+    "revision": "796f7d84d14aa26fabce",
+    "url": "/inbrief/static/js/29.ca87b60d.chunk.js"
+  },
+  {
+    "revision": "4f52e2deaa8c8b0eaf9b",
+    "url": "/inbrief/static/js/30.c69e24ba.chunk.js"
+  },
+  {
+    "revision": "551422771fcffca5c3d8",
+    "url": "/inbrief/static/js/31.93096035.chunk.js"
+  },
+  {
+    "revision": "697a53649204d5c4fb5a",
+    "url": "/inbrief/static/js/32.9affe3f1.chunk.js"
+  },
+  {
+    "revision": "0089577d44d2c4c7d2ad",
+    "url": "/inbrief/static/js/33.959fe6ae.chunk.js"
+  },
+  {
+    "revision": "e1f6fdf6c83bca52ae68",
+    "url": "/inbrief/static/js/34.52023dbf.chunk.js"
+  },
+  {
+    "revision": "e586eab71e38b75c86e1",
+    "url": "/inbrief/static/js/35.c45a306e.chunk.js"
+  },
+  {
+    "revision": "36d421baf294c6ba8083",
+    "url": "/inbrief/static/js/36.af5db70d.chunk.js"
+  },
+  {
+    "revision": "c47914dc82d9056ade11",
+    "url": "/inbrief/static/js/37.6c130015.chunk.js"
+  },
+  {
+    "revision": "e439ef3c9de9140e6f74",
+    "url": "/inbrief/static/js/38.202ac346.chunk.js"
+  },
+  {
+    "revision": "0dc811de2c2f4358408a",
+    "url": "/inbrief/static/js/39.f81dd02e.chunk.js"
+  },
+  {
+    "revision": "55cdd211976448d1c427",
+    "url": "/inbrief/static/js/4.e7c01cb4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/inbrief/static/js/4.e7c01cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2bd0bec44864679e9a",
+    "url": "/inbrief/static/js/40.21649259.chunk.js"
+  },
+  {
+    "revision": "3c7c7c363b0895a85d29",
+    "url": "/inbrief/static/js/41.d7cd38e5.chunk.js"
+  },
+  {
+    "revision": "fb98af4eedc801bf8650",
+    "url": "/inbrief/static/js/42.d98269d5.chunk.js"
+  },
+  {
+    "revision": "7a18d9ae579e60054c2e",
+    "url": "/inbrief/static/js/43.266834c8.chunk.js"
+  },
+  {
+    "revision": "1efd717945305066660b",
+    "url": "/inbrief/static/js/44.e291d9f1.chunk.js"
+  },
+  {
+    "revision": "1f75dbecf086fae48677",
+    "url": "/inbrief/static/js/45.6940272d.chunk.js"
+  },
+  {
+    "revision": "d2dcbbd35b36b20a86fb",
+    "url": "/inbrief/static/js/46.e49ea32c.chunk.js"
+  },
+  {
+    "revision": "ceae8ea62cfe75f68661",
+    "url": "/inbrief/static/js/47.2d74a41d.chunk.js"
+  },
+  {
+    "revision": "4ab8bfce295422401756",
+    "url": "/inbrief/static/js/48.e8499772.chunk.js"
+  },
+  {
+    "revision": "aeb40480e4ad4afb3c56",
+    "url": "/inbrief/static/js/49.1a07f69c.chunk.js"
+  },
+  {
+    "revision": "1404fda94b4bdaf90983",
+    "url": "/inbrief/static/js/5.222a3854.chunk.js"
+  },
+  {
+    "revision": "62e5e61cae0582f3a921",
+    "url": "/inbrief/static/js/50.537a1869.chunk.js"
+  },
+  {
+    "revision": "3403dac20d80bdfc177a",
+    "url": "/inbrief/static/js/51.cfa41493.chunk.js"
+  },
+  {
+    "revision": "376a445ac903cf04b8cf",
+    "url": "/inbrief/static/js/52.dbecf935.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/inbrief/static/js/52.dbecf935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30707eaf5b8c9a850a33",
+    "url": "/inbrief/static/js/53.88880af8.chunk.js"
+  },
+  {
+    "revision": "8aad76ebc097a27fc7f2",
+    "url": "/inbrief/static/js/54.6aaadac0.chunk.js"
+  },
+  {
+    "revision": "f630895f7af626b71cfc",
+    "url": "/inbrief/static/js/55.214f4f6c.chunk.js"
+  },
+  {
+    "revision": "10be0281c58cd5830877",
+    "url": "/inbrief/static/js/56.0a615c76.chunk.js"
+  },
+  {
+    "revision": "b83b87a52193ea7e3dea",
+    "url": "/inbrief/static/js/57.5efeb05b.chunk.js"
+  },
+  {
+    "revision": "1d9fd32a2ad702a8cd55",
+    "url": "/inbrief/static/js/58.f6bfb0a8.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/inbrief/static/js/58.f6bfb0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ee9177ff108c7dbc04",
+    "url": "/inbrief/static/js/6.978cf4f6.chunk.js"
+  },
+  {
+    "revision": "4a913ee27eede0c82657",
+    "url": "/inbrief/static/js/7.3f812aec.chunk.js"
+  },
+  {
+    "revision": "5110cacce816678d8805",
+    "url": "/inbrief/static/js/8.8b5adb3e.chunk.js"
+  },
+  {
+    "revision": "18536095775ee6b9d791",
+    "url": "/inbrief/static/js/9.0ab1036e.chunk.js"
+  },
+  {
+    "revision": "ba242881b5570fce6dba",
+    "url": "/inbrief/static/js/main.2175e857.chunk.js"
+  },
+  {
+    "revision": "dc6cef29aa32b670452a",
+    "url": "/inbrief/static/js/polyfills-css-shim.49c9290b.chunk.js"
+  },
+  {
+    "revision": "1a400a7cc8e5f56156ca",
+    "url": "/inbrief/static/js/runtime-main.fbff25a6.js"
+  }
+]);
